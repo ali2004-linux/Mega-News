615c581bc3f5cc5cd91c38eb6b59046259e5e244
ali2004-linux <ali.linux2004h@gmail.com>
2020-04-14 10:26:59 +0430

    wow

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//alert("سلام خدمت داوران دارکوب من علی همت نیا هستم که تازه با طراحی وب اشنا شدم تقریبا از مهر شروع کردم و وقتی سایت شما رو دیدم چند روز پیش یه فکر به سرم زد و گفتم این قالب ساده رو طرح کنم امیدوارم خوشتان بیاید علی همت نیا ۱۵ ساله از ارومیه")
+function menu(){
+    var m = document.getElementById('menu-');
+    var b = document.getElementById('ali');
+    b.style.display="none";
+    m.style.transform = "scaleX(1)";
+}
+function clo(){
+    var m = document.getElementById('menu-');
+    var b = document.getElementById('ali');
+    b.style.display="";
+    m.style.transform = "scaleX(0)";
+}
+function b() {
+	alert("هنوز اماده نیست ولی قسمت تماس با ما فعال است")
+}
+function s() {
+	alert("گفتم که هنوز ناقص!!!!!");
+}
